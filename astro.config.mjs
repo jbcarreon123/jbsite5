4c3a8f87cdc3524843f8cc4668522e6726116183
Jb Carreon <jbcarreon212@gmail.com>
2025-06-05 14:34:16 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+import nekoweb from '@indiefellas/astro-adapter-nekoweb';
+import { env } from 'node:process';
+
+let nkw = [];
+
+if (process.env.GITHUB_ACTIONS === 'true') {
+  nkw.push(nekoweb({
+    apiKey: env.NEKOWEB_APIKEY,
+    cookie: env.NEKOWEB_COOKIE,
+    folder: 'jbsite4_test'
+  }));
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jbcarreon123.nekoweb.org',
+    base: 'jbsite5',
+
+    integrations: [...nkw],
+
+    build: {
+        concurrency: 12
+      },
+    
+    image: {
+      service: passthroughImageService()
+    },
+});
